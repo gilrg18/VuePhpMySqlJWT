bdf1ba952e7e37ef4bf63dac941ea2fc358fa3b8
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-05-07 11:57:54 -0700

    Error messages

--- api/resources/views/users/create.blade.php
+++ api/resources/views/users/create.blade.php
@@ -7,6 +7,15 @@
 </head>
 <body>
     <h1>Create User</h1>
+    <div>
+        @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+            @endforeach
+        </ul>
+        @endif
+    </div>
     <form method="post" action="{{route('user.store')}}">
         @csrf
         @method('post')
