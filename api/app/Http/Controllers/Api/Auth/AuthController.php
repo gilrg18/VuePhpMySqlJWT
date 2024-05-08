53cd4e5db3dd1a9a7efc787abdf30979bc582f35
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-05-08 12:46:01 -0700

    Register request, jwt token missing, unique email rule

--- api/app/Http/Controllers/Api/Auth/AuthController.php
+++ api/app/Http/Controllers/Api/Auth/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegistrationRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -14,7 +15,27 @@ class AuthController extends Controller
     }
 
     //Register method
-    public function register(RegistrationRequest $request){
-        
+    public function register(RegistrationRequest $request)
+    {
+        $user = User::create($request->validated());
+        if($user){
+            $token = auth()->login($user);
+            return $this-> responseWithToken($token, $user);
+        }else{
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'An error occurred while trying to create user'
+            ], 500);
+        }
+    }
+
+    //Return jwt access token
+    public function responseWithToken($token, $user){
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'access_token' => $token,
+            'type' => 'bearer'
+        ]);
     }
 }
