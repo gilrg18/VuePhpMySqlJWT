5dbbc260a321c836065d7035c38b41fa97d1f0db
gilrg18 <gilberto_rogel_26@hotmail.com>
2024-05-08 10:55:55 -0700

    RegistrationRequest field rules

--- api/app/Http/Requests/RegistrationRequest.php
+++ api/app/Http/Requests/RegistrationRequest.php
@@ -11,7 +11,7 @@ class RegistrationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class RegistrationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required','string', 'min:2'],
+            'email' => ['required', 'email:filter'],
+            'password' => ['required', 'string', 'min:6', 'confirmed']
         ];
     }
 }
